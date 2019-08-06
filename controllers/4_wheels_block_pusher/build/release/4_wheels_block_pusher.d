14c502de672886ded6b683fa9dcd41fe146425f6
kylebrown <kylejbrown17@gmail.com>
2019-08-06 15:35:46 -0700

    Got two blocks to dynamically connect when pushed together by a robot! This is an important step toward constructing LEGO assemblies on the fly. Next steps: - Learn to "hack" the simulation so I can, for example, make blocks   float from the robot to the manufacturing station and vice versa. - Figure out how to get a robot to track a path.

---
+++ controllers/4_wheels_block_pusher/build/release/4_wheels_block_pusher.d
@@ -0,0 +1,6 @@
+build/release/4_wheels_block_pusher.o: 4_wheels_block_pusher.c \
+ /usr/local/webots/include/controller/c/webots/distance_sensor.h \
+ /usr/local/webots/include/controller/c/webots/types.h \
+ /usr/local/webots/include/controller/c/webots/motor.h \
+ /usr/local/webots/include/controller/c/webots/robot.h \
+ /usr/local/webots/include/controller/c/webots/nodes.h
