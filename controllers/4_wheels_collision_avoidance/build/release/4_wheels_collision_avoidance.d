717f6613d8540d7dbc5c87a514ca5be7e641f7d3
kylebrown <kylejbrown17@gmail.com>
2019-08-06 14:01:39 -0700

    Just completed custom robot tutorial. Very helpful tutorial indeed. Now on to learn how to turn the robot into a PROTO.

---
+++ controllers/4_wheels_collision_avoidance/build/release/4_wheels_collision_avoidance.d
@@ -0,0 +1,7 @@
+build/release/4_wheels_collision_avoidance.o: \
+ 4_wheels_collision_avoidance.c \
+ /usr/local/webots/include/controller/c/webots/distance_sensor.h \
+ /usr/local/webots/include/controller/c/webots/types.h \
+ /usr/local/webots/include/controller/c/webots/motor.h \
+ /usr/local/webots/include/controller/c/webots/robot.h \
+ /usr/local/webots/include/controller/c/webots/nodes.h
